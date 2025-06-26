d0b1caf9452dc59a94c16e99323fbc580190a2ec
Milan Singh <100085851+Mil9nn@users.noreply.github.com>
2025-06-26 13:49:36 +0530

    remove sentry

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
-import {withSentryConfig} from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withSentryConfig(nextConfig, {
-
-org: "js-mastery-lp",
-project: "javascript-nextjs",
-
-silent: !process.env.CI,
-
-widenClientFileUpload: true,
-
-tunnelRoute: "/monitoring",
-
-disableLogger: true,
-
-automaticVercelMonitors: true,
-});
+export default nextConfig;
